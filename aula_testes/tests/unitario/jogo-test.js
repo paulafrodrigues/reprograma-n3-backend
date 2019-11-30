64dad3fd7adbb3d8e8ce555c487dabe9e736edd3
Paula Franco <pfr.blackcat@gmail.com>
2019-11-30 16:04:02 -0300

    arrumando erros no teste unitário

---
+++ aula_testes/tests/unitario/jogo-test.js
@@ -0,0 +1,42 @@
+const chai = require('chai')
+const mocha = require('mocha')
+const jogo = require('../../jogo')
+const expect = chai.expect
+
+describe('Jogo | Unit', () => {
+    context('quando número gerado for maior que o número informado', () => {
+        it('retorna a mensagem "Tente um número menor!"', () => {
+            const numeroGerado = 50
+            const resposta = 51
+
+            const resultado = jogo(numeroGerado, resposta)
+
+            expect(resultado).to.be.an('object')
+            expect(resultado.mensagemErro).to.be.equal('Tente um número menor!')
+        })
+    })
+
+    context('quando o número gerado for menor que o número informado', () => {
+        it('retorna a mensagem "Tente um número maior!"', () => {
+            const numeroGerado = 50
+            const resposta = 46
+
+            const resultado = jogo(numeroGerado, resposta)
+
+            expect(resultado).to.be.an('object')
+            expect(resultado.mensagemErro).to.be.equal('Tente um número maior!')
+        })
+    })  
+
+    context('quando o número gerado for igual ao informado', () => {
+        it('retorna um objeto com a chave acertou como "true"', () => {
+            const numeroGerado = 30
+            const resposta = 30
+
+            const resultado = jogo(numeroGerado. resposta)
+
+            expect(resultado).to.be.an('object')
+            expect(resultado.acertou).to.be.true
+        })
+    })
+})
