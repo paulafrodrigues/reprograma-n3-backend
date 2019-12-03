da07e283ce221f4caa22b30e867584a7958a3b5b
Paula <pfr.blackcat@gmail.com>
2019-12-03 18:48:36 -0300

    mudanças em aula

--- aula2/routes/treinadores.js
+++ aula2/routes/treinadores.js
@@ -11,11 +11,7 @@ router.patch('/:id', controller.update)
 router.delete('/:id', controller.remove)
 router.post('/:treinadorId/pokemons', controller.addPokemon)
 router.patch('/:treinadorId/pokemons/:pokemonId/treinar', controller.treinarPokemon)
-<<<<<<< HEAD
-router.get('')
-=======
 router.get('/:treinadorId/pokemons/:pokemonId', controller.getPokemonById)
 router.patch('/:treinadorId/pokemons/:pokemonId', controller.updatePokemon)
->>>>>>> b54257e9794440fab7efa48b4995b8f9b9f44b1e
 
 module.exports = router
